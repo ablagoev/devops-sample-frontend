3e7e2c7fa993e463c550869c78028776c2722bd7
Alexander Blagoev <alexander.i.blagoev@gmail.com>
2022-01-22 10:11:39 +0200

    Lint errors

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import App from "./App";
 
 it("renders without crashing", () => {
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,7 +8,6 @@ import List from "@mui/material/List";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
-import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import MenuIcon from "@mui/icons-material/Menu";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
